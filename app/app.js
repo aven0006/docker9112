8d641f23545d71ba4d1e3183dc74df6934b925c1
Vladimir Cezar <vladimircezar@icloud.com>
2020-12-15 18:32:14 -0500

    Add backend

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let path = require('path');
 let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
 let bodyParser = require('body-parser');
 let app = express();
 
@@ -11,20 +12,55 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.post('/update-profile', function (req, res) {
+let mongoUrlLocal = "mongodb://admin:123456@localhost:27017";
+let databaseName = "students";
+let collectionName = "grades";
+
+app.post('/update', function (req, res) {
   let userObj = req.body;
 
-  // Send response
+  MongoClient.connect(mongoUrlLocal, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+    userObj['userid'] = 1;
+
+    let myquery = { userid: 1 };
+    let newvalues = { $set: userObj };
+
+    db.collection(collectionName).updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
+    });
+
+  });
   res.send(userObj);
 });
 
-app.get('/get-profile', function (req, res) {
-  res.send({})
+app.get('/get', function (req, res) {
+  let response = {};
+  
+  MongoClient.connect(mongoUrlLocal, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+
+    let myquery = { userid: 1 };
+
+    db.collection(collectionName).findOne(myquery, function (err, result) {
+      if (err) throw err;
+      response = result;
+      client.close();
+
+      res.send(response ? response : {});
+    });
+  });
 });
 
 app.listen(3000, function () {
   console.log("App docker9112 listening on port 3000!");
 });
+
